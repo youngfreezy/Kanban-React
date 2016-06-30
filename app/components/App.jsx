ca66d462cb4a311ac2616ad6758ba6179aaa5f6e
Fareez Ahmed <fareez.ahmed@ge.com>
2016-06-30 10:37:33 -0400

    pre redux refactor

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -22,8 +22,13 @@ export default class App extends React.Component {
     const {notes} = this.state;
     return (
       <div>
-        <button onClick={this.addNote}>+</button>
-        <Notes notes={notes} onDelete={this.deleteNote}/>
+        <button className="add-note" onClick={this.addNote}>+</button>
+        <Notes
+          notes={notes}
+          onNoteClick={this.activateNoteEdit}
+          onEdit={this.editNote}
+          onDelete={this.deleteNote}
+        />
       </div>
     );
   }
@@ -43,4 +48,24 @@ export default class App extends React.Component {
       notes: this.state.notes.filter(note => note.id !== id)
     })
   }
+
+  activateNoteEdit = (id) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = true;
+        }
+        return note;
+      })
+    }); }
+  editNote = (id, task) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = false;
+          note.task = task;
+        }
+        return note;
+      })
+    }); }
 }
